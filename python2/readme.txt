be68123e8f428285e1bfe7471e6c541879a7dc7f
urbanware-org <github@urbanware.org>
2018-04-26 13:08:06 +0200

    Add 'readme.txt' files for each version

---
+++ python2/readme.txt
@@ -0,0 +1,57 @@
+
+README (PaVal)
+
+    Project
+
+        PaVal
+        Version 1.2.7 (based on Python 2.x framework)
+        Copyright (C) 2018 by Ralf Kilian
+
+        GitHub: https://github.com/urbanware-org/paval
+
+    Definition
+
+        The PaVal module is an easy-to-use parameter validator for methods
+        inside Python scripts.
+
+    License
+
+        Permission is hereby granted, free of charge, to any person obtaining
+        a copy of this software and associated documentation files (the
+        "Software"), to deal in the Software without restriction, including
+        without limitation the rights to use, copy, modify, merge, publish,
+        distribute, sublicense, and/or sell copies of the Software, and to
+        permit persons to whom the Software is furnished to do so, subject to
+        the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+    Requirements
+
+        In order to use the module, Python 2.7 or higher is recommended, but
+        it may also work with earlier versions.
+
+    Usage
+
+        For fundamental documentation as well as some usage examples you may
+        have a look at the 'usage.txt' file.
+
+    Legal information
+
+        The project name is completely fictitious. Any correspondences with
+        existing websites, applications, companies and/or other projects are
+        purely coincidental.
+
+        All trademarks belong to their respective owners.
+
+        Errors and omissions excepted.
+
--- python3/paval.py
+++ python3/paval.py
@@ -16,47 +16,47 @@ import filecmp
 import os
 
 
-def compfile(path, name="", list_files=[]):
+def compfile(input_path, name="", list_files=None):
     """
         Compare files to avoid that the same file is given multiple times or
         in different ways (e. g. different name but same content).
     """
-    __string(path, "%s path" % name, True)
+    __string(input_path, "%s path" % name, True)
 
     if list_files is None:
         list_files = []
-    elif len(list_files) == 0:
+    elif not list_files:
         __ex("File list is empty (no files to compare with).", True)
     else:
         for item in list_files:
-            if not type(item) == list:
+            if not isinstance(item, list):
                 __ex("Every list item must be a sub-list.", True)
             if not len(item) == 2:
                 __ex("Every sub-list must contain two items.", True)
 
-    path = os.path.abspath(path)
+    input_path = os.path.abspath(input_path)
     for item in list_files:
         path_compare = os.path.abspath(str(item[0]))
         name_compare = str(item[1])
-        if path == path_compare:
+        if input_path == path_compare:
             __ex("The %s and the %s file path must not be identical." %
                  (name, name_compare), False)
-        if os.path.exists(path) and os.path.exists(path_compare):
-            if filecmp.cmp(path, path_compare, 0):
+        if os.path.exists(input_path) and os.path.exists(path_compare):
+            if filecmp.cmp(input_path, path_compare, 0):
                 __ex("The %s and %s file content must not be identical." %
                      (name, name_compare), False)
 
 
-def compstr(string, name="", list_strings=[]):
+def compstr(input_string, name="", list_strings=None):
     """
         Compare a string with a list of strings and check if it is an item of
         that list.
     """
-    __string(string, name, False)
-    if len(list_strings) == 0:
+    __string(input_string, name, False)
+    if not list_strings:
         __ex("No %s strings to compare with." % name, True)
-    if string not in list_strings:
-        __ex("The %s '%s' does not exist." % (name, string), False)
+    if input_string not in list_strings:
+        __ex("The %s '%s' does not exist." % (name, input_string), False)
 
 
 def get_version():
@@ -84,7 +84,7 @@ def intrange(value, name="", value_min=None, value_max=None, zero=False):
         if value_min > value_max:
             __ex("The maximal %s value must be greater than the minimal "
                  "value." % name, False)
-        if (value_min == value_max) and (not value == value_min):
+        if (value_min == value_max) and (value != value_min):
             __ex("The %s value can only be %s (depending on further range "
                  "further range arguments)." % (name, value_min), False)
         if (value < value_min) or (value > value_max):
@@ -117,43 +117,43 @@ def intvalue(value, name="", positive=True, zero=False, negative=False):
             __ex("The %s value must not be negative." % name, False)
 
 
-def path(path, name="", is_file=False, exists=False):
+def path(input_path, name="", is_file=False, exists=False):
     """
         Validate a path of a file or directory.
     """
-    string(path, "%s path" % name, False, None)
-    path = os.path.abspath(path)
+    string(input_path, "%s path" % name, False, None)
+    input_path = os.path.abspath(input_path)
 
     if is_file:
         path_type = "file"
     else:
         path_type = "directory"
     if exists:
-        if not os.path.exists(path):
+        if not os.path.exists(input_path):
             __ex("The given %s %s does not exist." % (name, path_type), False)
-        if (is_file and not os.path.isfile(path)) or \
-           (not is_file and not os.path.isdir(path)):
+        if (is_file and not os.path.isfile(input_path)) or \
+           (not is_file and not os.path.isdir(input_path)):
             __ex("The given %s %s path is not a %s." % (name, path_type,
                                                         path_type), False)
     else:
-        if os.path.exists(path):
+        if os.path.exists(input_path):
             __ex("The given %s %s path already exists." % (name, path_type),
                  False)
 
 
-def string(string, name="", wildcards=False, invalid_chars=None):
+def string(input_string, name="", wildcards=False, invalid_chars=None):
     """
         Validate a string.
     """
-    __string(string, name, False)
+    __string(input_string, name, False)
     if invalid_chars is None:
         invalid_chars = ""
     if not wildcards:
-        if ("*" in string) or ("?" in string):
+        if ("*" in input_string) or ("?" in input_string):
             __ex("The %s must not contain wildcards." % name, False)
-    if len(invalid_chars) > 0:
+    if invalid_chars:
         for char in invalid_chars:
-            if char in string:
+            if char in input_string:
                 # Use single quotes by default or double quotes in case the
                 # single quotes are the invalid character
                 quotes = "'"
@@ -164,16 +164,16 @@ def string(string, name="", wildcards=False, invalid_chars=None):
                      "(%s%s%s)." % (name, quotes, char, quotes), False)
 
 
-def __ex(string, internal=False):
+def __ex(exception_string, internal=False):
     """
         Internal method to raise an exception.
     """
-    string = str(string).strip()
-    while (" " * 2) in string:
-        string = string.replace((" " * 2), " ")
+    ex = str(exception_string).strip()
+    while " " * 2 in ex:
+        ex = ex.replace((" " * 2), " ")
     if internal:
-        string = "PaVal: " + string
-    raise Exception(string)
+        ex = "PaVal: " + ex
+    raise Exception(ex)
 
 
 def __integer(value, name="", internal=False):
@@ -191,13 +191,13 @@ def __integer(value, name="", internal=False):
     return int(value)
 
 
-def __string(string, name="", internal=False):
+def __string(input_string, name="", internal=False):
     """
         Internal method for basic string validation.
     """
-    if string is None:
+    if input_string is None:
         __ex("The %s is missing." % name, internal)
-    if string == "":
+    if input_string == "":
         __ex("The %s must not be empty." % name, internal)
 
 # EOF
